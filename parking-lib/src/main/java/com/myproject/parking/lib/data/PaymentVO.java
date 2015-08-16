49661a27eb1dec2891580df319030b1baffbafb4
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-08-17 00:47:25 +0700

    Tambah Fitur Payment With Credit Card

---
+++ parking-lib/src/main/java/com/myproject/parking/lib/data/PaymentVO.java
@@ -0,0 +1,33 @@
+package com.myproject.parking.lib.data;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+public class PaymentVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Address address;
+	private CreditCard creditCard;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+}
