359c3ac29ef1fa7bd5dae1b0388047f7ca649b7a
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-08-20 23:51:36 +0700

    a

---
+++ parking-lib/src/main/java/com/myproject/parking/lib/mapper/MallMapper.java
@@ -0,0 +1,11 @@
+package com.myproject.parking.lib.mapper;
+
+import java.util.List;
+
+import com.myproject.parking.lib.entity.Mall;
+
+public interface MallMapper {
+	
+	public List<Mall> findAllMall();
+	
+}
