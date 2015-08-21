a56305ab26014c1cbee0f8643363c879943e9195
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-08-21 11:56:55 +0700

    a

--- parking-lib/src/main/java/com/myproject/parking/lib/service/LoginService.java
+++ parking-lib/src/main/java/com/myproject/parking/lib/service/LoginService.java
@@ -1,5 +1,8 @@
 package com.myproject.parking.lib.service;
 
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,10 @@ public class LoginService {
 	
 	@Autowired
 	private AppsTimeService timeService;
+	
+	@Autowired
+	private CheckSessionKeyService checkSessionKeyService;
+	
 	
 	public LoginData login(LoginData loginData) throws ParkingEngineException {
 		LOG.debug("login with param : " + " loginData: " + loginData );	
@@ -52,6 +59,35 @@ public class LoginService {
 		loginData.setPassword("*******");
 		return loginData;
 //		LOG.info("login done with param : " + " loginData: " + loginData);
+	}
+	
+	public void logout(LoginData loginData) throws ParkingEngineException {
+		LOG.debug("logout with param : " + " loginData: " + loginData );	
+		UserData user = userDataMapper.findUserDataByEmail(loginData.getEmail());
+		if(user == null){
+			LOG.error("Can't find User with email : " + loginData.getEmail());
+			throw new ParkingEngineException(ParkingEngineException.ENGINE_USER_NOT_FOUND);
+		}
+		if(Constants.BLOCKED == user.getStatus()){
+			LOG.error("User already blocked");
+			throw new ParkingEngineException(ParkingEngineException.ENGINE_USER_BLOCKED);
+		}
+		if(Constants.PENDING == user.getStatus()){
+			LOG.error("User not active");
+			throw new ParkingEngineException(ParkingEngineException.ENGINE_USER_NOT_ACTIVE);
+		}		
+		if(StringUtils.isEmpty(user.getSessionKey())){
+			LOG.error("User Must Login Before make transaction, Parameter email : " + loginData.getEmail());
+			throw new ParkingEngineException(ParkingEngineException.ENGINE_USER_NOT_LOGIN);
+		}
+		if(!user.getSessionKey().equals(loginData.getSessionKey())){
+			LOG.error("Wrong Session Key, Parameter email : " + loginData.getEmail());
+			throw new ParkingEngineException(ParkingEngineException.ENGINE_SESSION_KEY_DIFFERENT);
+		}		
+		checkSessionKeyService.checkSessionKey(user.getTimeGenSessionKey(), loginData.getEmail());
+		Date now = timeService.getCurrentTime();
+		userDataMapper.removeSessionKey(user.getId(), null, now);
+		
 	}
 	
 }
