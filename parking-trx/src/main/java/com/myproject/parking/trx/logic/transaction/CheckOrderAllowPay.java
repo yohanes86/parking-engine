ef45f8ee0cba06097e0161d7d7ffc23b3be07eb5
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-08-26 13:48:24 +0700

    a

---
+++ parking-trx/src/main/java/com/myproject/parking/trx/logic/transaction/CheckOrderAllowPay.java
@@ -0,0 +1,53 @@
+// #CreateCreditCard Sample
+// Using the 'vault' API, you can store a 
+// Credit Card securely on PayPal. You can
+// use a saved Credit Card to process
+// a payment in the future.
+// The following code demonstrates how 
+// can save a Credit Card on PayPal using 
+// the Vault API.
+// API used: POST /v1/vault/credit-card
+package com.myproject.parking.trx.logic.transaction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.myproject.parking.lib.data.BookingVO;
+import com.myproject.parking.lib.service.BookingService;
+import com.myproject.parking.lib.service.ParkingEngineException;
+import com.myproject.parking.lib.utils.MessageUtils;
+import com.myproject.parking.trx.logic.BaseQueryLogic;
+
+public class CheckOrderAllowPay implements BaseQueryLogic {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CheckOrderAllowPay.class);
+	
+	@Autowired
+	private BookingService bookingService;
+
+	@Override
+	public String process(HttpServletRequest request,HttpServletResponse response,String data, ObjectMapper mapper, String pathInfo) {
+		LOG.debug("Start process Query :"+pathInfo);		
+		String result = "";
+		try {						
+			BookingVO bookingVO = mapper.readValue(data, BookingVO.class);
+			bookingService.checkBookingIdAllowPay(bookingVO);
+			result = MessageUtils.handleSuccess("Booking Id : " + bookingVO.getBookingId() + " allowed to pay." , mapper);
+		} catch (ParkingEngineException e) {
+			result = MessageUtils.handleException(e, e.getMessage(), mapper);
+			LOG.error("ParkingEngineException when processing " + pathInfo + " Error Message : " + result);
+		} catch (Exception e) {
+			result = MessageUtils.handleException(e, "Unexpected exception when processing "+ e.getMessage(), mapper);
+			LOG.error("Unexpected exception when processing " + pathInfo + " Error Message " + result, e);
+		}
+		return result;
+	}
+	
+	
+
+}
